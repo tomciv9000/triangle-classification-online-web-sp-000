f8c0d75615977186fc6b4411f752115b35e99c06
Tom White <eugene.white@gmail.com>
2019-06-19 04:30:47 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,9 +3,9 @@ class Triangle
   attr_reader :side_1, :side_2, :side_3
   
   def initialize(side_1, side_2, side_3)
-    @side1 = side_1
-    @side2 = side_2
-    @side3 = side_3
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
   end
   
   def kind 

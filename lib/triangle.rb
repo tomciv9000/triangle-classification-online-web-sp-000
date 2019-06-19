ae9496622ef3319180967cafc85b978b224cb69b
Tom White <eugene.white@gmail.com>
2019-06-19 03:56:19 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,14 +1,15 @@
 class Triangle
   
-  attr_reader :side1, :side2, :side3
+  attr_reader :side_1, :side_2, :side_3
   
-  def initialize(side1, side2, side3)
-    @side1 = side1
-    @side2 = side2
-    @side3 = side3
+  def initialize(side_1, side_2, side_3)
+    @side1 = side_1
+    @side2 = side_2
+    @side3 = side_3
   end
   
   def kind 
-    if side1 != side2 && side1 != side3 
-  
+    if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
+      raise TriangleError
+    elsif 
 end

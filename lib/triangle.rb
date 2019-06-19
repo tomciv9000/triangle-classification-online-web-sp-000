cea75023d54e26208e23857a5c23b169888a2652
Tom White <eugene.white@gmail.com>
2019-06-19 04:19:24 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,5 +13,5 @@ class Triangle
       raise TriangleError
     elsif (@side_1 == @side_2) && (@side2 == @side3)
       :equilateral
-    elsif (@side_1 == @side_2)
+    elsif (@side_1 == @side_2) && (@side2 != @)
 end
